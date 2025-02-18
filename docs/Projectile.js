e78f8192aa925b7396a0336f4c4ae3d1311f2aa3
XuanpuGuo <uw24974@bristol.ac.uk>
2025-02-18 16:58:29 +0000

    Projectile.js

---
+++ docs/Projectile.js
@@ -0,0 +1,13 @@
+class Projectile {
+   constructor(x, y, angle, speed,duration) {
+    this.x = x;
+    this.y = y;
+    this.duration = duration;
+   }
+   draw(){
+     
+   }
+  update(){
+    
+  }
+}
