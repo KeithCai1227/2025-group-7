dc96751420f3a3adce96daf89bb4cce260fedb95
xDyasser <111101422+xDyasser@users.noreply.github.com>
2025-02-18 14:51:17 +0000

    Fixing walls.js file

--- Prototype/walls.js
+++ Prototype/walls.js
@@ -8,7 +8,7 @@ let brickColors = ['#ab744f', '#a1591e', '#9c5536', '#7e411f', '#963f19'];
 function setMap() {
   for(let row = 0; row < rows; row++){
     for(let col = 0; col < cols; col++){
-      if(map3[row][col] === 1){
+      if(map1[row][col] === 1){
         let tile = new tiles.Sprite(col * tileSize + tileSize / 2, row * tileSize + tileSize / 2, tileSize, tileSize);
 
         tile.color = random(brickColors);
