0ec34e8fc25ca6fe9357031e72774320c4dabe98
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-02-27 09:21:59 +0000

    Adding keycode variable for firing of tank

--- docs/TankTrouble.js
+++ docs/TankTrouble.js
@@ -1,9 +1,8 @@
 //global declaration of GameState object
 let tankGame;
 
-//spacebar is keycode 32
-let SPACEBAR_CODE = 32;
-
+//key codes for firing of tanks
+let ZERO_NUMPAD_CODE = 96;
 let Q_CODE = 81;
 
 function setup() {
@@ -16,7 +15,7 @@ function draw() {
 }
 
 function keyPressed() {
-    if (keyCode === 96 && tankGame.tankList[0].canFire() && !tankGame.getIsGameOver()){
+    if (keyCode === ZERO_NUMPAD_CODE && tankGame.tankList[0].canFire() && !tankGame.getIsGameOver()){
         tankGame.addProjectile(tankGame.tankList[0].fire());
     }
     if (keyCode === Q_CODE && tankGame.tankList[1].canFire() && !tankGame.getIsGameOver()) {
