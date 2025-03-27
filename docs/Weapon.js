26a5785d872124859390240473d03fd0359d8b98
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-03-27 19:59:47 +0000

    Changes to Weapon class for bomb type

--- docs/Weapon.js
+++ docs/Weapon.js
@@ -2,10 +2,21 @@ class Weapon{
     numberOfRounds; //Number of projectiles which have been fired so far
     capacity; //Total number of projectiles which can be fired
     weaponType; //Takes different values depending upon weapon type
+
+    //enumeration of weapon types
     static BULLET_TYPE = 0;
     static LASER_TYPE = 1;
+    static BOMB_TYPE = 2;
+
+    //capacity of weapon types
     static BULLET_CAPACITY = 10;
     static LASER_CAPACITY = 1;
+    static BOMB_CAPACITY = 1;
+
+    //time before weapon expires
+    static BULLET_TIME = 10;
+    static BOMB_TIME = 5;
+    static SPLINTER_TIME = 5;
     
     constructor(weaponType){
         if(weaponType == Weapon.BULLET_TYPE){
@@ -16,6 +27,11 @@ class Weapon{
         else if(weaponType == Weapon.LASER_TYPE){
             this.weaponType = weaponType;
             this.capacity = Weapon.LASER_CAPACITY;
+            this.numberOfRounds = 0;
+        }
+        else if(weaponType == Weapon.BOMB_TYPE){
+            this.weaponType = weaponType;
+            this.capacity = Weapon.BOMB_CAPACITY;
             this.numberOfRounds = 0;
         }
     }
