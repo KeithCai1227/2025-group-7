8a832285ca0be5624c85ecf846998655276da3b0
xDyasser <111101422+xDyasser@users.noreply.github.com>
2025-04-17 11:45:48 +0000

    Refactored some classes for readability and error handling

--- docs/Projectile.js
+++ docs/Projectile.js
@@ -1,8 +1,15 @@
+/**
+ * Abstract base class for all projectiles.
+ * Defines common properties like position, angle, and lifespan.
+ * Subclasses must implement draw(), update(), and remove() methods.
+ */
+
 class Projectile {
 
     constructor(x, y, angle, duration){
 		this.x = x;
 		this.y = y;
+		this.angle = angle;
 		this.duration = duration;
 		this.despawnTime = millis() + (this.duration * 1000);
 	}
--- docs/Weapon.js
+++ docs/Weapon.js
@@ -1,57 +1,65 @@
-class Weapon{
-    numberOfRounds; //Number of projectiles which have been fired so far
-    capacity; //Total number of projectiles which can be fired
-    weaponType; //Takes different values depending upon weapon type
+class Weapon {
+    numberOfRounds; // Number of projectiles which have been fired so far
+    capacity; // Total number of projectiles which can be fired
+    weaponType; // Takes different values depending upon weapon type
 
-    //enumeration of weapon types
+    // Enumeration of weapon types
     static BULLET_TYPE = 0;
     static LASER_TYPE = 1;
     static BOMB_TYPE = 2;
     static SAW_TYPE = 3;
 
-    //capacity of weapon types
+    // Capacity of weapon types
     static BULLET_CAPACITY = 10;
     static LASER_CAPACITY = 1;
     static BOMB_CAPACITY = 1;
 
-    //time before weapon expires
+    // Time before weapon expires
     static BULLET_TIME = 10;
-    static LASER_TIME = 8; 
+    static LASER_TIME = 8;
     static BOMB_TIME = 5;
     static SPLINTER_TIME = 2;
-    
-    constructor(weaponType){
-        if(weaponType == Weapon.BULLET_TYPE){
-            this.weaponType = weaponType;
-            this.capacity = Weapon.BULLET_CAPACITY;
-            this.numberOfRounds = 0;
-            this.fireSound = audioBulletShot;
-        }
-        else if(weaponType == Weapon.LASER_TYPE){
-            this.weaponType = weaponType;
-            this.capacity = Weapon.LASER_CAPACITY;
-            this.numberOfRounds = 0;
-            this.fireSound = audioLaserShot;
-        }
-        else if(weaponType == Weapon.BOMB_TYPE){
-            this.weaponType = weaponType;
-            this.capacity = Weapon.BOMB_CAPACITY;
-            this.numberOfRounds = 0;
-            this.fireSound = audioBombShot;
-        }
-        else if(weaponType == Weapon.SAW_TYPE){
-            this.weaponType = weaponType;
-            this.capacity = null;
-            this.numberOfRounds = null;
-            this.fireSound = audioLaserShot;
-        }
-    }
 
-    getAmmo() {
-        return this.capacity - this.numberOfRounds;
-    }
-
-    resetAmmo() {
+    /**
+     * Creates a Weapon instance based on the provided type.
+     */
+    constructor(weaponType) {
+        this.weaponType = weaponType;
         this.numberOfRounds = 0;
-    } 
+
+        switch (weaponType) {
+            case Weapon.BULLET_TYPE:
+                this.capacity = Weapon.BULLET_CAPACITY;
+                this.fireSound = audioBulletShot;
+                break;
+            case Weapon.LASER_TYPE:
+                this.capacity = Weapon.LASER_CAPACITY;
+                this.fireSound = audioLaserShot;
+                break;
+            case Weapon.BOMB_TYPE:
+                this.capacity = Weapon.BOMB_CAPACITY;
+                this.fireSound = audioBombShot;
+                break;
+            case Weapon.SAW_TYPE:
+                this.capacity = null;
+                this.numberOfRounds = null;
+                this.fireSound = audioLaserShot; 
+                break;
+            default:
+                throw new Error("Invalid weapon type");
+        }
+    }
+    // Get remaining ammo
+    getAmmo() {
+        if (this.capacity === null || this.numberOfRounds === null) {
+            return null; 
+        }
+        return Math.max(0, this.capacity - this.numberOfRounds);
+    }
+    // Reset ammo
+    resetAmmo() {
+        if (this.numberOfRounds !== null) {
+            this.numberOfRounds = 0;
+        }
+    }
 }
